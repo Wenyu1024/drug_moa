5400999d01262422501e0aadf598b1872cb831c7
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-05-04 07:42:22 +0000

    update reverse modeling server code

--- sensitivity_fitting/reverse_modelling_server.sh
+++ sensitivity_fitting/reverse_modelling_server.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH -p small
 #SBATCH --cpus-per-task=40
-#SBATCH --mem-per-cpu=100
+#SBATCH --mem-per-cpu=1000
 
 # Load r-env-singularity
 module load r-env-singularity/4.0.2
