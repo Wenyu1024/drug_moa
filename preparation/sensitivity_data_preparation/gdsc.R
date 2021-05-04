70bc139ab71f9d839fa870fba5a64319b541f837
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-05-04 09:20:54 +0000

    init

---
+++ preparation/sensitivity_data_preparation/gdsc.R
@@ -0,0 +1,7 @@
+# prepare first the whole dataset but with ids that can later be matched to ctd2.
+# I will run first on gdsc independently. 
+# Do I have to run all the analysis on GDSC? perhaps first only do the sensitivity fitting.
+# and then if the reviewer asked, do the rest.
+
+# to run the analysis I need to map cell id. basically map Sanger_Model_ID to depmap_iD
+# using the sample_info_broad.csv file
