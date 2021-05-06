59f820a8b25228778b7df99bde9ccd7d6d39a8a5
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-05-06 13:26:10 +0000

    spearman used instead of previous metrics and also changes in data structure

--- sensitivity_fitting/function/train_and_pred.R
+++ sensitivity_fitting/function/train_and_pred.R
@@ -10,12 +10,11 @@ train_and_pred <- function(df){
   ceres_columns <- str_subset(string = colnames(df$training_data[[1]]), pattern = "_ceres")
   demeter_columns <- str_subset(string = colnames(df$training_data[[1]]), pattern = "_demeter")
   ces_columns <- str_subset(string = colnames(df$training_data[[1]]), pattern = "_ces")
-  # if (is_empty(ceres_columns) | is_empty(demeter_columns) )  next
+
   formula_ceres <- as.formula(str_c("area_under_curve ~ ", str_c(ceres_columns, collapse = " + "), collapse = " "))
   formula_demeter2 <- as.formula(str_c("area_under_curve ~ ", str_c(demeter_columns, collapse = " + "), collapse = " "))
   formula_ces <- as.formula(str_c("area_under_curve ~ ", str_c(ces_columns, collapse = " + "), collapse = " "))
-  # formula_all <- as.formula(area_under_curve ~ .)
-  
+
   tmp <-   df %>% 
     mutate(fit_lm_ceres  = map(training_data, ~fit(lm_model, formula_ceres, data = .x))) %>% 
     mutate(fit_lm_demeter2  = map(training_data, ~fit(lm_model, formula_demeter2, data = .x))) %>% 
@@ -30,30 +29,14 @@ train_and_pred <- function(df){
                               pred_demeter2=third$.pred,
                               pred_ces=fourth$.pred,                             
                        )}
-    )) 
+    )) %>%
+    select(id, res) %>%
+    unnest(res) %>%
+    pivot_longer(cols = 3:5,names_to= "method", values_to= "pred") %>% 
+    group_by(id,method) %>%
+    summarise(cor= cor(real, pred, method = "spearman")) %>% 
+    ungroup() 
   
-  
-  get_perf <- function(x){
-    x1= enquo(x)
-    tmp %>%
-      select(id, res) %>%
-      unnest(res) %>%
-      group_by(id) %>%
-      metrics(truth = real, estimate = !!x1) %>%
-      ungroup() %>%
-      select(-3)
-  }
-  
-  perf_ceres <- get_perf("pred_ceres") %>% rename(ceres = .estimate)
-  perf_demeter2 <- get_perf("pred_demeter2") %>% rename(demeter = .estimate)
-  perf_ces<- get_perf("pred_ces") %>% rename(ces = .estimate)
-  
-  res <-  perf_ceres %>%
-    inner_join(perf_demeter2,by = c("id", ".metric")) %>%
-    inner_join(perf_ces,by = c("id", ".metric"))
-  return(res)
-  
-  # tmp <- tmp %>% mutate(ceres= perf_cer)
-  # return(tmp)
+  return(tmp)
 }
 
