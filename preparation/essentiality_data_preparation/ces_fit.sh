115c0336c41dc748355ae6bbeaa76579e4803432
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-06-03 13:48:18 +0000

    delete generate_ces_iput_21q1.R code, since this local code was added to the generate_cesinput_long_format_puhti.R to allow running on the server code to make the process more straightforward

--- preparation/essentiality_data_preparation/ces_fit.sh
+++ preparation/essentiality_data_preparation/ces_fit.sh
@@ -3,10 +3,11 @@
 #SBATCH --account=project_2003466
 #SBATCH --output=output_%j.txt
 #SBATCH --error=errors_%j.txt
-#SBATCH --time=02:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=60000
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=5000
 
 # Load r-env-singularity
 module load r-env-singularity/4.0.2
--- preparation/essentiality_data_preparation/fit_ces_and_impute.R
+++ preparation/essentiality_data_preparation/fit_ces_and_impute.R
@@ -1,16 +1,17 @@
 setwd("/scratch/project_2003466/ces_21q1_io/")
 library(tidyverse)
-data <- read_csv("ces_input_21q1.csv")
+# data <- read_csv("ces_input_21q1.csv")
 
 # install.packages("speedglm")
 #cells <- unique(data$DepMap_ID)
 #data1 <- data %>% dplyr::filter(DepMap_ID %in% cells[1:20])
 
 # ptm <- proc.time()
-lm <- lm(ceres~ demeter2+mut+exp_seq+cn+exp_array+DepMap_ID ,data = data)
-pred <- predict.lm(lm)
-
-save.image("tmp.RData")
+# lm <- lm(ceres~ demeter2+mut+exp_seq+cn+exp_array+DepMap_ID ,data = data)
+# pred <- predict.lm(lm)
+# 
+# save.image("tmp.RData")
+load("tmp.RData")
 # tmp = proc.time() - ptm
 # print(tmp)
 data <- data %>% 
@@ -20,7 +21,7 @@ data <- data %>%
 ces1_437 <- data %>% select(DepMap_ID, gene, ces1) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= ces1)
 ceres_437 <- data %>% select(DepMap_ID, gene, ceres) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= ceres)
 demeter2_437 <- data %>% select(DepMap_ID, gene, demeter2) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= demeter2)
-expseq_437 <- data %>% select(DepMap_ID, gene, ceres) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= expseq_437)
+expseq_437 <- data %>% select(DepMap_ID, gene, exp_seq ) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= expseq_437)
 
 impute_ess_df <- 
   function(df){
@@ -43,4 +44,4 @@ write_csv(ces1_437, "ces1_21q1_imputed.csv")
 write_csv(ceres_437, "ceres_21q1_imputed.csv")
 write_csv(demeter2, "demeter2_21q1_imputed.csv")
 write_csv(expseq_437, "expseq_21q1_imputed.csv")
-save.image("tmp.RData")
+save.image("tmp1.RData")
