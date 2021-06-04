688a08aec1c41c39a42f7595585fcff23e9a4227
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-06-04 08:44:01 +0000

    remove genes that are missing too much before the imputation

--- preparation/essentiality_data_preparation/fit_ces_and_impute.R
+++ preparation/essentiality_data_preparation/fit_ces_and_impute.R
@@ -17,11 +17,20 @@ load("tmp.RData")
 data <- data %>% 
   select_at(c(1:4,6)) %>% 
   mutate(ces1= pred) 
-  
+
+# Only include genes whose observation over 3/4  
+gene_list <- data %>% 
+  group_by(gene) %>% 
+  summarize(n_obs= n()) %>% 
+  ungroup() %>% 
+  filter(n_obs > length(unique(data$DepMap_ID))*0.75 ) %>% 
+  pull(gene)
+
+data <- data %>% filter(gene %in% gene_list)
 ces1_437 <- data %>% select(DepMap_ID, gene, ces1) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= ces1)
 ceres_437 <- data %>% select(DepMap_ID, gene, ceres) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= ceres)
 demeter2_437 <- data %>% select(DepMap_ID, gene, demeter2) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= demeter2)
-expseq_437 <- data %>% select(DepMap_ID, gene, exp_seq ) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= expseq_437)
+expseq_437 <- data %>% select(DepMap_ID, gene, exp_seq ) %>% pivot_wider(id_cols = DepMap_ID, names_from= gene, values_from= exp_seq)
 
 impute_ess_df <- 
   function(df){
@@ -33,15 +42,15 @@ impute_ess_df <-
     df1 <- bind_cols(df[,1], as.data.frame(imputed_X))
     return(df1)
   }
-set.seed(1234)
 
-ces1_437 <- impute_ess_df(ces1_437)
-ceres_437 <- impute_ess_df(ceres_437)
-demeter2_437 <- impute_ess_df(demeter2_437)
-expseq_437 <-  impute_ess_df(expseq_437)
-
-write_csv(ces1_437, "ces1_21q1_imputed.csv")
-write_csv(ceres_437, "ceres_21q1_imputed.csv")
-write_csv(demeter2, "demeter2_21q1_imputed.csv")
-write_csv(expseq_437, "expseq_21q1_imputed.csv")
 save.image("tmp1.RData")
+set.seed(1234)
+ces1_437 <- impute_ess_df(ces1_437)
+write_csv(ces1_437, "ces1_21q1_imputed.csv")
+ceres_437 <- impute_ess_df(ceres_437)
+write_csv(ceres_437, "ceres_21q1_imputed.csv")
+demeter2_437 <- impute_ess_df(demeter2_437)
+write_csv(demeter2, "demeter2_21q1_imputed.csv")
+expseq_437 <-  impute_ess_df(expseq_437)
+write_csv(expseq_437, "expseq_21q1_imputed.csv")
+save.image("tmp2.RData")
