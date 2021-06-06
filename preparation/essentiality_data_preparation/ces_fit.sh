90e35f6292550823ea1f070f727442224d55841a
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-06-06 08:53:12 +0000

    ces fitting and predictor matrix imputation finished

--- preparation/essentiality_data_preparation/ces_fit.sh
+++ preparation/essentiality_data_preparation/ces_fit.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem-per-cpu=5000
+#SBATCH --mem-per-cpu=4000
 
 # Load r-env-singularity
 module load r-env-singularity/4.0.2
