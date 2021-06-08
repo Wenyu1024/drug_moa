df4b005acbfed761a47242e22ba06f813f3c0391
Wenyu1024 <wenyu.wang@helsinki.fi>
2021-06-08 07:34:44 +0000

    Add the preparation code for gdsc dataset

--- sensitivity_fitting/forward_modelling/prepare_inputRdata.R
+++ sensitivity_fitting/forward_modelling/prepare_inputRdata.R
@@ -7,7 +7,7 @@
 # save.image("~/cluster_scratch/glmnet_modelling_cluster/input_test.RData")
 # 
 
-load("~/cluster_scratch/ctrp/ctrpv2_data.RData")
+# load("~/cluster_scratch/ctrp/ctrpv2_data.RData")
 setwd("~/cluster_scratch/ces_21q1_io")
 ces1<- read_csv("./ces1_21q1_imputed.csv")
 ceres  <- read_csv("./ceres_21q1_imputed.csv")
@@ -15,7 +15,15 @@ demeter2 <- read_csv("./demeter2_21q1_imputed.csv")
 # exp_seq_pca <- read_csv("~/cluster_scratch/impute_and_derive_exp_based_feature_importance/exp_seq_pca.csv")
 exp_seq <- read_csv("./expseq_21q1_imputed.csv")
 
-data <- ctrpv2_data %>%
+# data <- ctrpv2_data %>%
+#   ungroup() 
+# rm(ctrpv2_data)
+# save.image("~/cluster_scratch/forward_modelling/ctrp_input.RData")
+
+
+load("~/cluster_scratch/gdsc/gdsc2.RData")
+data <- gdsc_df %>%
   ungroup() 
-rm(ctrpv2_data)
-save.image("~/cluster_scratch/forward_modelling/ctrp_input.RData")
+rm(gdsc_df)
+save.image("~/cluster_scratch/forward_modelling/gdsc_input.RData")
+
