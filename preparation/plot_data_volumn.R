9c8913dd021799ff0b20f9304e13a9e9698bb576
Wenyu1024 <wenyu.wang@helsinki.fi>
2022-10-18 08:11:16 +0000

    data volume analysis plan (abandoned)

---
+++ preparation/plot_data_volumn.R
@@ -0,0 +1,38 @@
+#This code is used to generate a circular bar plot to show L1000 has less
+# gene and cells.
+# https://www.data-to-viz.com/graph/circularbarplot.html#:~:text=Definition,than%20a%20long%20usual%20barplot.
+# not used in the end
+
+# CRISPR gene=  , cell= 
+# shRNA gene= , cell=
+# L1000 gene
+# PANACEA gene
+
+# cell
+# CTRP
+# GDSC
+# PRISM
+# CTRP-crispr
+# GDSC-crispr
+# PRISM-crispr
+# CTRP-shRNA
+# GDSC-shRNA
+# PRISM-shRNA
+# L1000
+# PANACEA
+
+
+# compound =
+
+# overlapping cell median sen 
+
+# CTRP-crispr
+# GDSC-crispr
+# PRISM-crispr
+# 
+# CTRP-shRNA
+# GDSC-shRNA
+# PRISM-shRNA
+
+# L1000 drug, cell_median gene
+# PANACEA drug cell_median gene
